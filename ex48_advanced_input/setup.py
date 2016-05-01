f6f229cfcad31b42d484f480ffcbe42962d2c810
Qbicz <avenger.v14@gmail.com>
2016-05-02 00:09:47 +0200

    project skeleton with tests

---
+++ ex48_advanced_input/setup.py
@@ -0,0 +1,22 @@
+try:
+    from setuptools import setup
+    print("setuptools setup")
+except ImportError:
+    from distutils.core import setup
+    print("distutils.core setup")
+    
+config = {
+    'description': 'Advanced User Input - lexicon',
+    'author': 'Filip Kubicz',
+    'url': 'http://github.com/Qbicz',
+    'download_url': 'where to download',
+    'author_email': 'avenger.v14@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['lexicon'],
+    'scripts': [],
+    'name': 'lexicon'
+ }
+
+setup(**config)
+ 
