2ddf3ceb13c4ee959058bc5c29ca378da0735a11
Qbicz <avenger.v14@gmail.com>
2016-05-02 01:15:56 +0200

    unit tests set up

---
+++ ex48_advanced_input/lexicon.py
@@ -0,0 +1,10 @@
+import logging
+
+class lexicon(object):
+        
+    def scan():
+        """scan function for ..."""
+        logging.debug('scan:')
+        
+    def scan(string):
+        pass
